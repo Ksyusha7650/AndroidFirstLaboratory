753522d3916d18a1bd916574f7ded91e7b8d0d1f
Ksu <ksysha.2002r@gmail.com>
2023-09-26 23:33:26 +0300

    add comments and handler empty strings exception

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -14,47 +14,64 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText editTextA, editTextB, editTextAngle;
     TextView textViewResult;
-    double A, B, angle;
+    double A, B, angle; // две стороны и угол треугольника
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        connectUIElements();
+        connectUIElements(); // связывает компоненты пользовательского интерфейса с переменными класса
     }
 
     private void outputErrorDialog(String errorText) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle(getString(R.string.error))
-                .setMessage(errorText)
-                .setPositiveButton(getString(R.string.ok), (dialog, id) -> dialog.cancel());
-        builder.show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this); // создает диалоговое окно
+        builder.setTitle(getString(R.string.error)) // устанавливает заголовок диалогового окна
+                .setMessage(errorText) // устанавливает текст сообщения в диалоговом окне
+                .setPositiveButton(getString(R.string.ok), (dialog, id) -> dialog.cancel()); // устанавливает кнопку "OK" и ее обработчик
+        builder.show(); // отображает диалоговое окно
     }
 
     private void connectUIElements() {
-        setContentView(R.layout.activity_main);
-        editTextA = findViewById(R.id.editTextSide1);
+        setContentView(R.layout.activity_main); // устанавливает макет для активности
+        editTextA = findViewById(R.id.editTextSide1); // находит элементы пользовательского интерфейса по их id
         editTextB = findViewById(R.id.editTextSide2);
         editTextAngle = findViewById(R.id.editTextAngle);
         textViewResult = findViewById(R.id.textViewResult);
     }
 
     private void setValuesFromUIElements(){
-        A = Double.parseDouble(editTextA.getText().toString());
-        B = Double.parseDouble(editTextB.getText().toString());
-        angle = Integer.parseInt(editTextAngle.getText().toString());
-        if (angle >= 180) throw new ArithmeticException(getString(R.string.error_angle));
-        if (A == 0) throw new ArithmeticException(getString(R.string.error_side));
-        if (B == 0) throw new ArithmeticException(getString(R.string.error_side));
-        if (angle == 0) throw new ArithmeticException(getString(R.string.error_angle));
+        String textA = editTextA.getText().toString();
+        String textB = editTextB.getText().toString();
+        String textAngle = editTextAngle.getText().toString();
+
+        if (textA.isEmpty()) {
+            throw new ArithmeticException(getString(R.string.error_empty_field_a)); // выбрасывает исключение, если сторона A не введена
+        }
+
+        if (textB.isEmpty()) {
+            throw new ArithmeticException(getString(R.string.error_empty_field_b)); // выбрасывает исключение, если сторона B не введена
+        }
+
+        if (textAngle.isEmpty()) {
+            throw new ArithmeticException(getString(R.string.error_empty_field_angle)); // выбрасывает исключение, если угол не введен
+        }
+
+        A = Double.parseDouble(textA);
+        B = Double.parseDouble(textB);
+        angle = Integer.parseInt(textAngle);
+        if (angle >= 180) throw new ArithmeticException(getString(R.string.error_angle)); // выбрасывает исключение, если угол >= 180
+        if (A == 0) throw new ArithmeticException(getString(R.string.error_side)); // выбрасывает исключение, если сторона A равна 0
+        if (B == 0) throw new ArithmeticException(getString(R.string.error_side)); // выбрасывает исключение, если сторона B равна 0
+        if (angle == 0) throw new ArithmeticException(getString(R.string.error_angle)); // выбрасывает исключение, если угол равен 0
     }
 
     public void buttonCalculateClick(View view) {
         try{
-            setValuesFromUIElements();
-            double square = getSquare();
-            textViewResult.setText(String.valueOf((double) Math.round(square * 100) / 100)); // с точностью до 2 знаков после запятой
+            setValuesFromUIElements(); // задает значения переменных A, B, angle на основе введенных пользователем данных
+            double square = getSquare(); // вычисляет площадь треугольника
+            textViewResult.setText(String.valueOf((double) Math.round(square * 100) / 100)); // выводит результат на экран с округлением до 2 знаков после запятой
         }
         catch (ArithmeticException | NumberFormatException exception){
-            outputErrorDialog(exception.getMessage());
+            outputErrorDialog(exception.getMessage()); // выводит диалоговое окно с сообщением об ошибке
         }
     }
 
